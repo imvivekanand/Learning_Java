c3d1b987ffb48afc270cb39b2dc2c8ec302793e8
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-03-20 13:33:40 +0530

    Operators v2

---
+++ src/com/collections/java/Main.java
@@ -0,0 +1,4 @@
+package com.collections.java;
+
+public class Main {
+}
--- src/com/staticclass/java/StaticKeyword.java
+++ src/com/staticclass/java/StaticKeyword.java
@@ -1,4 +1,30 @@
 package com.staticclass.java;
 
 public class StaticKeyword {
+    public static void main(String[] args){
+        Employee e1 = new Employee();
+        e1.name = "Vivek";
+
+        System.out.println(Employee.company);
+
+        Employee.count = 20;
+        System.out.println(Employee.count);
+
+        Employee.myFirstStaticMethod();
+    }
+
+}
+
+class Employee{
+    public String name; //It will vary for each employee object.
+
+    static final String company = "QualiMe"; //Constant value. It belongs to Class not to a object.
+    public static  int count = 0;
+
+    public static void myFirstStaticMethod(){ //Class Method
+        System.out.println("My first static method.");
+        System.out.println(Employee.count);
+    }
+
+
 }
